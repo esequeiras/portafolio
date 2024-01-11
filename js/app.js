2b9fb116efae5f96b0bc207fb0b84a24099476e5
esequeiras <esequeiras@ucenfotec.ac.cr>
2024-01-10 22:27:50 -0600

    arreglos

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+/*document.addEventListener('DOMContentLoaded', function () {
     var imgContainer = document.querySelector('.grande');
 
     var currentIndex = 0; // Índice de la imagen actual
@@ -19,3 +19,56 @@ document.addEventListener('DOMContentLoaded', function () {
         imgContainer.style.transform = 'translateX(' + translateValue + ')';
     }
 });
+function redirigir() {
+    window.location.href = '#projects';
+}*/
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtener referencias a los elementos
+    var checkbox = document.getElementById("btn-modal");
+    var modal = document.getElementById("myModal");
+
+    // Función para actualizar la visibilidad del modal
+    function updateModalVisibility() {
+        if (checkbox.checked) {
+            modal.style.visibility = "visible";
+            modal.style.opacity = 1;
+        } else {
+            modal.style.visibility = "hidden";
+            modal.style.opacity = 0;
+        }
+    }
+
+    // Escuchar cambios en el estado del checkbox
+    checkbox.addEventListener("change", updateModalVisibility);
+
+    // Llamar a la función inicialmente para establecer el estado inicial
+    updateModalVisibility();
+     // Agrega la función para cambiar el contenido al hacer clic en el botón "Siguiente"
+     document.getElementById("adelante").addEventListener("click", cambiarContenido);
+    });
+    
+    let indiceImagen = 0;
+    
+    const imagenes = [
+        { src: "assets/login.png", texto: "Bloom cuenta con diversas secciones diseñadas específicamente para ayudar a personas que sufren de ansiedad." },
+        { src: "assets/dash.png", texto: "Dashboard donde el usuario puede ver graficos sobre sus niveles y ataques de ansiedad desde que inició a usar la app." },
+        { src: "assets/juegos.png", texto: "Sección de juegos" },
+        { src: "assets/carta.png", texto: "Juego de cartas, se puede cambiar la dificultad que va desde facil (10 cartas) a dificil (28 cartas)" },
+        { src: "assets/romp.png", texto: "Juego puzzle, se puede cambiar la dificultad, imagen"},
+        { src: "assets/foro.png", texto: "Foro donde se pueden compartir experiancias mediante comentarios y publicaciones "},
+        { src: "assets/chat.png", texto: "Chat implementado con web sokets"},
+        { src: "assets/calen.png", texto: "Calendario, se utilizó un job para notificar al usuario cuando esté próximo a un evento"},
+        { src: "assets/gpt.png", texto: "Diario emocional, se implementó chat GPT para dar inspiración al usuario sobre qué escribir"},
+
+
+        // Agrega más objetos con rutas de imágenes y textos según sea necesario
+    ];
+    
+    function cambiarContenido() {
+        indiceImagen = (indiceImagen + 1) % imagenes.length;
+        console.log(imagenes[indiceImagen].src)
+        console.log(imagenes[indiceImagen].texto)
+
+        document.getElementById("imagenModal").src = imagenes[indiceImagen].src;
+        document.getElementById("textoModal").innerText = imagenes[indiceImagen].texto;
+    }
